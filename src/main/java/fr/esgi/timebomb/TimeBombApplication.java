db033197af7edf263b4ae1f7cab5b7b13a0e472a
Mvestro <nicolasgarnier97@gmail.com>
2021-05-21 15:30:00 +0200

    add struct project

---
+++ src/main/java/fr/esgi/timebomb/TimeBombApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.timebomb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeBombApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeBombApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/esgi/timebomb/TimeBombApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.timebomb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeBombApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
